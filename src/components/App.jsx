61493454df8a898dd54934b5c69830326e853df7
Anddy <alonzoa2015@gmail.com>
2022-07-25 22:51:41 -0700

    added stuff to temnplate forgot to commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Layout from './Layout/Layout.jsx';
+import Home from './Home/Home.jsx';
+import Contact from './Contact/Contact.jsx';
+import About from './About/About.jsx';
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import '../reset.css';
+import styles from './Home.css';
+
+
+export default function Home() {
+  return (
+    <div className={styles.Home}>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Pellentesque euismod, nisi eu consectetur consectetur,
+        nisl nisl consectetur nisl, eu consectetur nisl nisl euismod
+        nisl.
+
+        Pellentesque euismod, nisi eu consectetur consectetur,
+        nisl nisl consectetur nisl, eu consectetur nisl nisl euismod
+        nisl.
+
+        Pellentesque euismod, nisi eu consectetur consectetur,
+        nisl nisl consectetur nisl, eu consectetur nisl nisl euismod
+        nisl.
+      </p>
+
+
+    </div>
+  );
+}
