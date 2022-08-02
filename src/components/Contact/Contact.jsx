ef297785a1f8e6d9e36868bbcdea90dbd1d18710
Anddy <alonzoa2015@gmail.com>
2022-08-02 16:52:41 -0700

    added all the imports i think ill need

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,15 @@
+import styles from './Header.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import { useTheme } from '@mui/material/styles';
+import { useState } from 'react';
+
 export default function Contact() {
   return <p>contact stuff</p>;
 }
