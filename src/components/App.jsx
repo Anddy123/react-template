b12a28217bb9652014cfd4cf1bef09ea0c246d2c
Anddy <alonzoa2015@gmail.com>
2022-08-07 21:43:43 -0700

    added the start of the fuzzy bunnies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,19 +8,25 @@ import Layout from './Layout/Layout.jsx';
 import Home from './Home/Home.jsx';
 import Contact from './Contact/Contact.jsx';
 import Pokedex from './Pokedex/Pokedex.jsx';
+import FuzzyBunny from './FuzzyBunny/FuzzyBunny.jsx';
+import FuzzyBunnyProvider from './state/context/fuzzybunnycontext.jsx';
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="pokedex" element={<Pokedex />} />
-          <Route path="fuzzybunny" element={<FuzzyBunny />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+      <Toaster />
+      <FuzzyBunnyProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="pokedex" element={<Pokedex />} />
+            <Route path="fuzzybunny" element={<FuzzyBunny />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </FuzzyBunnyProvider>
     </Router>
   );
 }
