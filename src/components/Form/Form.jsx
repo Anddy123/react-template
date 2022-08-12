b49e99ec0af21dbdb9a1f99fec48fe5c8b9cc784
Anddy <alonzoa2015@gmail.com>
2022-08-12 16:41:43 -0700

    typing to fast

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-mport { Children, cloneElement, forwardRef } from 'react';
+import { Children, cloneElement, forwardRef } from 'react';
 import classNames from 'classnames';
 import styles from './FormControls.css';
 
